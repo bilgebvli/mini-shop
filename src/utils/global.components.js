a49570a01869350f725938a9b73aa55a8122a949
bilgebavli <bilgebvli@gmail.com>
2021-11-07 16:53:02 +0300

    Components were added globally

---
+++ src/utils/global.components.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+
+import MiniShopCounter from "@/components/controls/MiniShopCounter";
+import MiniShopNotification from "@/components/ui/MiniShopNotification";
+import MiniShopButton from "@/components/ui/MiniShopButton";
+import MiniShopPageLoader from "@/components/ui/MiniShopPageLoader";
+import MiniShopPreloader from "@/components/ui/MiniShopPreloader";
+import MiniShopCard from "@/components/ui/MiniShopCard";
+
+Vue.component("MiniShopCounter", MiniShopCounter);
+Vue.component("MiniShopNotification", MiniShopNotification);
+Vue.component("MiniShopButton", MiniShopButton);
+Vue.component("MiniShopPageLoader", MiniShopPageLoader);
+Vue.component("MiniShopPreloader", MiniShopPreloader);
+Vue.component("MiniShopCard", MiniShopCard);
