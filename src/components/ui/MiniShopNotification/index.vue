e99a3492291c01905d66c22f38008fb1b267f64e
bilgebavli <bilgebvli@gmail.com>
2021-11-07 16:45:03 +0300

    Notification component was created

---
+++ src/components/ui/MiniShopNotification/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <notifications class="cursor-pointer" v-bind="notificationOptions" />
+</template>
+<script>
+export default {
+  name: "MiniShopNotification",
+  props: {
+    group: {
+      type: String,
+      default: "general",
+    },
+  },
+  computed: {
+    notificationOptions() {
+      return {
+        group: this.group,
+        position: "top right",
+        width: 350,
+        classes: "vue-notification mt-4 p-3 my-style",
+        closeOnClick: true,
+      };
+    },
+  },
+};
+</script>
+<style lang="scss">
+.my-style {
+  font-size: 15px;
+}
+</style>
