a14a0fc19fe86b1bcc11ac0c6406e0ead68c8937
bilgebavli <bilgebvli@gmail.com>
2021-11-07 16:38:28 +0300

    Counter component was created

---
+++ src/components/controls/MiniShopCounter/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="counter">
+      <button
+        class="counter__button"
+        :disabled="disabledDecrementButton"
+        @click="decrement"
+      >
+        <i class="bi bi-plus"></i>
+      </button>
+      <input type="number" class="counter__input" :value="number" disabled />
+      <button
+        class="counter__button"
+        :disabled="disabledIncrementButton"
+        @click="increment"
+      >
+        <i class="bi bi-plus"></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MiniShopCounter",
+  props: {
+    value: {
+      type: [Number, String],
+      default: 0,
+    },
+    min: {
+      type: Number,
+      default: 0,
+    },
+    max: {
+      type: Number,
+      default: 100,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    number: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+        this.$emit("change", value);
+      },
+    },
+    disabledDecrementButton() {
+      return this.disabled || this.number - 1 < this.min;
+    },
+    disabledIncrementButton() {
+      return this.disabled || this.number + 1 > this.max;
+    },
+  },
+  methods: {
+    decrement() {
+      this.number = this.number - 1;
+    },
+    increment() {
+      this.number = this.number + 1;
+    },
+  },
+};
+</script>
