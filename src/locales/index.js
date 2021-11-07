f6558084d9f03a7cdf79d06c30a9622cca8b8c0c
bilgebavli <bilgebvli@gmail.com>
2021-11-07 16:04:14 +0300

    Locales file was created

---
+++ src/locales/index.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import VueI18n from "vue-i18n";
+
+const numberFormats = {
+  "tr-TR": {
+    currency: {
+      style: "currency",
+      currency: "TRY",
+      currencyDisplay: "symbol",
+      minimumIntegerDigits: 1,
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 2,
+      useGrouping: false,
+    },
+  },
+};
+Vue.use(VueI18n);
+
+const i18n = new VueI18n({
+  locale: "tr-TR",
+  numberFormats,
+});
+
+export default i18n;
