16ce4019d28f10e510deb7b90c2b0dff290855ff
bilgebavli <bilgebvli@gmail.com>
2021-11-07 16:05:33 +0300

    Mixins files were created

---
+++ src/mixins/global.store.mixin.js
@@ -0,0 +1,12 @@
+const globalStoreMixin = {
+  methods: {
+    async showAppLoading() {
+      await this.$store.commit("setIsAppLoading", { isAppLoading: true });
+    },
+    async hideAppLoading() {
+      await this.$store.commit("setIsAppLoading", { isAppLoading: false });
+    },
+  },
+};
+
+export default globalStoreMixin;
---
+++ src/utils/global.mixins.js
@@ -0,0 +1,6 @@
+import Vue from "vue";
+import GlobalMixin from "@/mixins/global.mixin";
+import GlobalStoreMixin from "@/mixins/global.store.mixin";
+
+Vue.mixin(GlobalMixin);
+Vue.mixin(GlobalStoreMixin);
