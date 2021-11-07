0ad472d2e05b12afb7430266d4395c1d744f55d7
bilgebavli <bilgebvli@gmail.com>
2021-11-07 16:25:44 +0300

    Store was created for basket

---
+++ src/store/modules/basket/actions.js
@@ -0,0 +1,29 @@
+import api from "@/api";
+
+export default {
+  async fetchBasket({ commit }) {
+    const item = await api
+      .get("https://6182f8f991d76c00172d171e.mockapi.io/api/v1/basket")
+      .then((res) => res.data);
+    commit("setBasket", { basket: item });
+  },
+  async addBasket(context, { basket }) {
+    return await api.post(
+      "https://6182f8f991d76c00172d171e.mockapi.io/api/v1/basket",
+      {
+        ...basket,
+      }
+    );
+  },
+  async editBasket(context, { productId = null, basket }) {
+    return await api.put(
+      `https://6182f8f991d76c00172d171e.mockapi.io/api/v1/basket/${productId}`,
+      { ...basket }
+    );
+  },
+  async deleteBasket(context, { productId = null }) {
+    return await api.delete(
+      `https://6182f8f991d76c00172d171e.mockapi.io/api/v1/basket/${productId}`
+    );
+  },
+};
---
+++ src/store/modules/basket/mutations.js
@@ -0,0 +1,5 @@
+export default {
+  setBasket(state, { basket = [] }) {
+    state.basket = basket;
+  },
+};
