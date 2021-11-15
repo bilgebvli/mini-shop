cec51cd0e18e0d8189320fb6941eae216628ec58
bilgebavli <bilgebvli@gmail.com>
2021-11-16 02:35:37 +0300

    Global image component was created

---
+++ src/components/ui/MiniShopImage/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <img v-bind="imageOptions" />
+</template>
+
+<script>
+export default {
+  name: 'MiniShopImage',
+  props: {
+    src: {
+      type: String,
+      default: null,
+    },
+    alt: {
+      type: String,
+      default: null,
+    },
+    lazy: {
+      type: Boolean,
+      default: false,
+    },
+    width: {
+      type: [Number, String],
+      default: null,
+    },
+    height: {
+      type: [Number, String],
+      default: null,
+    },
+  },
+  computed: {
+    imageOptions() {
+      return {
+        src: this.src,
+        alt: this.alt,
+        lazy: this.lazy,
+        width: this.width,
+        height: this.height,
+      };
+    },
+  },
+};
+</script>
